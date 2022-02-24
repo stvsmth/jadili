b941f692434845e3cb75161ce48181cda320491b
stvsmth <stvsmth@gmail.com>
2022-02-24 13:20:47 -0700

    Minor change to test deploy.

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -184,7 +184,7 @@ fn action_buttons() -> RawHtmlEl {
             action_button("run", "Create 1,000 rows", || create_rows(1_000)),
             action_button("runlots", "Create 10,000 rows", || create_rows(10_000)),
             action_button("add", "Append 1,000 rows", || append_rows(1_000)),
-            action_button("update", "Update every 10th row", || update_rows(10)),
+            action_button("update", "Update every 3rd row", || update_rows(3)),
             action_button("clear", "Clear", clear_rows),
             action_button("swaprows", "Swap Rows", swap_rows),
         ]))
